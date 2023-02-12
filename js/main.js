e588d4f76fcbe35b4231b1b0da5efd03cf87a7d1
lulasp <luis.scsp@gmail.com>
2023-02-12 16:47:24 +0000

    update main.js add toggle active & show hide function

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@ const faqQuestions = document.querySelectorAll(".faq-question__wrapper");
 faqQuestions.forEach(question => {
   question.addEventListener("click", function() {
     console.log("click event");
+    this.classList.toggle("active");
+    const faqAnswer = this.nextElementSibling;
+    if (faqAnswer.style.maxHeight) {
+      faqAnswer.style.maxHeight = null;
+    } else {
+      faqAnswer.style.maxHeight = `${faqAnswer.scrollHeight}px`;
+    }
   });
 });
