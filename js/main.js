8b489abbb381260df6658e185729aceb111faca8
lulasp <luis.scsp@gmail.com>
2023-02-12 22:48:13 +0000

    remove comments and console.log

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const faqQuestions = document.querySelectorAll(".faq-question__wrapper");
 
 faqQuestions.forEach(question => {
   question.addEventListener("click", function() {
-    console.log("click event");
     this.classList.toggle("active");
     const faqAnswer = this.nextElementSibling;
     if (faqAnswer.style.maxHeight) {
